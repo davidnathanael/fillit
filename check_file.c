76a684bc35b09383f2632e0bc77cda70a540324a
David nathanael DELA CRUZ <ddela-cr@e3r9p10.42.fr>
2015-12-02 18:58:50 +0100

    Nothing interesting, just to make sure it's saved. Bye

--- check_file.c
+++ check_file.c
@@ -6,7 +6,7 @@
 /*   By: ddela-cr <ddela-cr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 12:49:51 by ddela-cr          #+#    #+#             */
-/*   Updated: 2015/12/02 16:03:25 by ddela-cr         ###   ########.fr       */
+/*   Updated: 2015/12/02 18:58:11 by ddela-cr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,9 @@ char	*ft_file2str(int fd)
 	int		ret;
 	char	buf[BUF_SIZE + 1];
 
-	while ((ret = read(fd, buf, BUF_SIZE)))
-	{
-		ft_putnbr(ret);
-		ft_putchar('\n');
-		if (ret < BUF_SIZE - 1)
-			return (NULL);
-		buf[ret] = '\0';
-	}
-	ft_putnbr(ret);
-	return (ft_strdup("test"));
+	ret = read(fd, buf, BUF_SIZE);
+	buf[ret] = '\0';
+	if (ret <= 0)
+		return (NULL);
+	return (ft_strdup(buf));
 }
--- check_file.h
+++ check_file.h
@@ -6,14 +6,14 @@
 /*   By: ddela-cr <ddela-cr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 13:50:19 by ddela-cr          #+#    #+#             */
-/*   Updated: 2015/12/02 14:17:41 by ddela-cr         ###   ########.fr       */
+/*   Updated: 2015/12/02 18:31:50 by ddela-cr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECK_FILE_H
 # define CHECK_FILE_H
 
-# define BUF_SIZE 21
+# define BUF_SIZE 1024
 # define VALID 1
 # define NOT_VALID -1
 
